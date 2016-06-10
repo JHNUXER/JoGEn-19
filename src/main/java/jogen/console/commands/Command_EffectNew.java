3d439181858be5079430b5670d06e7e34718d477
John Kayne <johnhoganuke2@gmail.com>
2016-06-10 15:18:46 -0400

    This is where my problem lies

---
+++ src/main/java/jogen/console/commands/Command_EffectNew.java
@@ -0,0 +1,7 @@
+package jogen.console.commands;
+
+import jogen.console.Command;
+
+public class Command_EffectNew extends Command {
+  public void function(String charName, ) {}    // Use the function in my question. This one is not finished. I need to figure out how to get this to work right first
+}
